2a1e1d7e63acb474c619e7f2e1c5ae706535c448
waagabriel <166228832+waagabriel@users.noreply.github.com>
2024-06-15 20:06:13 -0300

    Add files via upload

--- letras.py
+++ letras.py
@@ -1,6 +1,6 @@
 import requests
 import sys
-
+import json
 artista = input("\x1b[31mInsira o nome do Artista:\t")
 musica  = input("\x1b[38;5;18mInsira o nome da musica:\t")
 
@@ -9,11 +9,12 @@ Api = f"https://api.lyrics.ovh/v1/{artista}/{musica}"
 data = requests.get(Api)
 
 if data.status_code == 404:
-    print("\x1b[38;5;52mErro ao pesquisar\n")
+    print("Erro ao pesquisar\n")
     sys.exit(1)
 
 letras = str(data.content)
-letras = letras.replace('\\n', '\n').replace('\\r', ' ').replace('\\', ' ')
+letras = json.loads(data.content)
+# letras = letras.replace('\\n', '\n').replace('\\r', ' ').replace('\\', ' ')
 
 
-print("\x1b[93m ",letras)
+print("\x1b[93m ",letras['lyrics'])
